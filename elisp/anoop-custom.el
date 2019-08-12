2abdd454266c2e4ed94df41b3950211e2b57aae4
Anoop Johnson <jinsumv@Jinsus-MacBook-Air.local>
2019-08-12 10:52:23 -0700

    Fix

--- elisp/anoop-custom.el
+++ elisp/anoop-custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(load-home-init-file t t)
- )
+ '(package-selected-packages (quote (dracula-theme use-package ensime))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
