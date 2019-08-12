75fe5013f94bc57f20187132bd23e7aa6c0ab651
Anoop Johnson <anoopj@amazon.com>
2019-08-12 10:30:05 -0700

    Upd

---
+++ create-symlinks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DEST=$PWD
+ln -sf  $DEST/dotbashrc ~/.bashrc
+ln -sf $DEST/dotbashrc-Darwin ~/.bashrc-Darwin
+ln -sf $DEST/dotbashrc-Linux  ~/.bashrc-Linux
+ln -sf $DEST/dotemacs ~/.emacs
+ln -sf $DEST/dotgnus ~/.gnus
+ln -sf $DEST/dotvim ~/.vim
+ln -sf $DEST/dotvimrc ~/.vimrc
+ln -sf $DEST/elisp ~/elisp
